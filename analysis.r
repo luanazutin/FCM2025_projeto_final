94e70898d94456db7b478bcb1b6fcff97afb6fef
luanazutin <luanazutin1@gmail.com>
2025-11-20 17:24:25 -0300

    Criei a pasta graficos, adicionei comando no inicio do script para isso e adicionei ggsave para todos os graficos.

--- analysis.r
+++ analysis.r
@@ -10,14 +10,20 @@ head(df)
 colnames(df)
 
 ##brainstorm##
+#criar pasta para salvar os gráficos
+if (!dir.exists("graficos")) {
+  dir.create("graficos")
+}
+
 # verificar quantidade de gêneros e espécies 
 nrow(df) # 57 spp
 
 contagem_gen <- df %>% 
   count(Genus) %>%
   mutate(porcentagem = round(n / sum(n) * 100, digits = 1))
-#gráfico de pizza
-ggplot(contagem_gen, aes(x = "", y = n, fill = Genus)) +
+
+#gráfico de pizza - atribuindo a varíavel
+grafico_generos <- ggplot(contagem_gen, aes(x = "", y = n, fill = Genus)) +
   geom_bar(stat = "identity", position = "stack") +
   coord_polar(theta = "y") +
   theme_void()+
@@ -35,7 +41,7 @@ ggplot(contagem_gen, aes(x = "", y = n, fill = Genus)) +
     legend.text = element_text(face = "italic", size = 12)
   )
 
-ggsave("primeiro_grafico.png")
+ggsave("graficos/grafico_generos.png", plot = grafico_generos, width = 8, height = 6)
 
 #Status de conservaçao - por genero? por distribuicao geografica? 
 
@@ -43,7 +49,7 @@ dados_ameacados <- df %>%
   count(Genus, X2024.IUCN.Red.List.category) %>% 
   filter(X2024.IUCN.Red.List.category %in% c("VU", "EN", "CR"))
 
-ggplot(dados_ameacados, aes(x = Genus, y = n, fill = X2024.IUCN.Red.List.category)) +
+grafico_status_ameaca <- ggplot(dados_ameacados, aes(x = Genus, y = n, fill = X2024.IUCN.Red.List.category)) +
   geom_bar(stat = "identity") +
   coord_flip() +
   theme_minimal() +
@@ -59,6 +65,8 @@ ggplot(dados_ameacados, aes(x = Genus, y = n, fill = X2024.IUCN.Red.List.categor
     fill = "Status de ameaça IUCN"
   )
 
+ggsave("graficos/grafico_status_ameaca.png", plot = grafico_status_ameaca, width = 8, height = 6)
+
 
 #visão geral da massa corporal
 df <- df %>% 
@@ -67,24 +75,27 @@ df <- df %>%
 summary(df$Average.Mass)
 
 #histograma
-ggplot(df, aes (x = Average.Mass))+
+grafico_massa_hist <- ggplot(df, aes (x = Average.Mass))+
   geom_histogram(bins = 15)+
   theme_minimal()
+ggsave("graficos/grafico_massa_hist.png", plot = grafico_massa_hist, width = 8, height = 6)
 
 #boxplot
-ggplot(df, aes(x = "", y = Average.Mass)) +
+grafico_massa_box <- ggplot(df, aes(x = "", y = Average.Mass)) +
   geom_boxplot() +
   theme_classic()
+ggsave("graficos/grafico_massa_box.png", plot = grafico_massa_box, width = 8, height = 6)
 
+#média massa
 media_massa_gen <- df %>%
   group_by(Genus) %>%
   summarise(media_massa = mean(Average.Mass, na.rm = TRUE))
 
-ggplot(media_massa_gen, aes(x = Genus, y = media_massa, fill = Genus)) +
+grafico_media_massa_gen <- ggplot(media_massa_gen, aes(x = Genus, y = media_massa, fill = Genus)) +
   geom_bar(stat = "identity") +
   scale_fill_brewer(palette = "Paired")+
   coord_flip() +
-    labs(
+  labs(
     title = "Média da massa por gêneros",
     x = "Gêneros",
     y = "Média da massa corporal"
@@ -95,13 +106,15 @@ ggplot(media_massa_gen, aes(x = Genus, y = media_massa, fill = Genus)) +
     axis.text.y = element_text(face = "italic", size = 12),
     legend.position = "none"
   )
-# library(RColorBrewer)
-# display.brewer.all()
-
-
-#visão geral sobre ocorrencias
-#contagem de Restricted range
-df %>% 
+ggsave("graficos/grafico_media_massa_gen.png", plot = grafico_media_massa_gen, width = 8, height = 6)**
+  
+  #library(RColorBrewer)
+  #display.brewer.all()
+  
+  
+  #visão geral sobre ocorrencias
+  #contagem de Restricted range
+  df %>% 
   count(RR)
 
 #contagem de species' breeding is restricted to island(s) (seria endêmicas)
@@ -123,42 +136,45 @@ df %>%
 
 str(df)
 
+#altitude média por gênero
 media_alt_gen <- df %>%
   group_by(Genus) %>%
   summarise(media_altitude = mean(Elevational.Range, na.rm = TRUE))
 
-ggplot(media_alt_gen, aes(y = media_altitude, x = Genus)) +  
+grafico_altitude_gen <- ggplot(media_alt_gen, aes(y = media_altitude, x = Genus)) +  
   geom_bar(stat = "identity", fill = "steelblue") +
   theme_minimal()+
   theme(axis.text.x = element_text(face = "italic"))
-  
-ggplot(media_alt_gen, aes(x = media_altitude)) +
-  geom_histogram(bins = 15, fill = "steelblue")
+ggsave("graficos/grafico_altitude_gen.png", plot = grafico_altitude_gen, width = 8, height = 6)
+
+#histograma da altitude média por gênero
+grafico_altitude_hist <- ggplot(media_alt_gen, aes(x = media_altitude)) +
+  geom_histogram(bins = 15, fill = "steelblue") +
+  theme_minimal() +
+  labs(
+    title = "Histograma: Altitude média por gênero",
+    x = "Altitude média",
+    y = "Contagem"
+  )
+ggsave("graficos/grafico_altitude_hist.png", plot = grafico_altitude_hist, width = 8, height = 6)
 
 #boxplot
-ggplot(df, aes(x = Genus, y = Elevational.Range, fill = Genus)) +
+grafico_altitude_box <- ggplot(df, aes(x = Genus, y = Elevational.Range, fill = Genus)) +
   geom_boxplot() +
   scale_fill_brewer(palette = "Paired")+
   theme_classic() +
   theme(axis.text.x = element_text(face = "italic"),
         legend.position = "none")
+ggsave("graficos/grafico_altitude_box.png", plot = grafico_altitude_box, width = 8, height = 6)
 
-#Uso de habitat
-df %>%
-  count(Primary.Habitat)
-
-uso_habitat <- df %>% 
-  count(Primary.Habitat) %>%
+#Uso de habitat - gráfico de pizza
+uso_habitat <- df %>%
+  count(Primary.Habitat)%>%
   mutate(porcentagem = round(n / sum(n) * 100, digits = 1))
 
-
-#gráfico de pizza
-ggplot(uso_habitat, aes(x = "", y = n, fill = Primary.Habitat)) +
+grafico_habitat <- ggplot(uso_habitat, aes(x = "", y = n, fill = Primary.Habitat)) +
   geom_bar(stat = "identity", position = "stack") +
   coord_polar(theta = "y") +
-  scale_fill_discrete(labels = c("Forest" = "Floresta",
-                      "Shrub" = "Arbusto",
-                      "Woodland" = "Bosque"))+
   scale_fill_manual(values = c(
     "Forest" = "green",
     "Shrub" = "yellow",
@@ -174,17 +190,15 @@ ggplot(uso_habitat, aes(x = "", y = n, fill = Primary.Habitat)) +
     legend.text = element_text(size = 12)
   )
 
-#Dieta
-df %>%
-  count(Primary.Diet)
+ggsave("graficos/grafico_habitat_pizza.png", plot = grafico_habitat, width = 8, height = 6)
 
-dieta <- df %>% 
-  count(Primary.Diet) %>%
+#Dieta
+dieta <- df %>%
+  count(Primary.Diet)%>%
   mutate(porcentagem = round(n / sum(n) * 100, digits = 1))
 
-
 #gráfico de pizza
-ggplot(dieta, aes(x = "", y = n, fill = Primary.Diet)) +
+grafico_dieta <- ggplot(dieta, aes(x = "", y = n, fill = Primary.Diet)) +
   geom_bar(stat = "identity", position = "stack") +
   coord_polar(theta = "y") +
   scale_fill_discrete(labels = c("Fruit" = "Fruta",
@@ -207,9 +221,11 @@ ggplot(dieta, aes(x = "", y = n, fill = Primary.Diet)) +
     legend.text = element_text(size = 12)
   )
 
+ggsave("graficos/grafico_dieta.png", plot = grafico_dieta, width = 8, height = 6)
+
 #amplitude da dieta
 #boxplot
-ggplot(df, aes(x = Genus, y = DB, fill = Genus)) +
+grafico_dieta_boxplot <- ggplot(df, aes(x = Genus, y = DB, fill = Genus)) +
   geom_boxplot() +
   scale_fill_brewer(palette = "Paired")+
   theme_classic() +
@@ -226,7 +242,7 @@ especialidade <- df %>%
     TRUE ~ "Especialistas"
   ))
 #geral
-ggplot(especialidade, aes(x = "", y = ESI, fill = nível_especialidade)) +
+grafico_ESI_geral <- ggplot(especialidade, aes(x = "", y = ESI, fill = nível_especialidade)) +
   geom_bar(stat = "identity", position = "stack") +
   coord_polar(theta = "y") +
   scale_fill_brewer(palette = "Set3") +
@@ -234,11 +250,12 @@ ggplot(especialidade, aes(x = "", y = ESI, fill = nível_especialidade)) +
   labs(
     fill = "Nível de especialidade ecológica", x = "", y = ""
   ) 
-  
+ggsave("graficos/grafico_ESI_geral.png", plot = grafico_ESI_geral, width = 8, height = 6)
+
 #por gênero
-ggplot(especialidade, 
-       aes(x = Genus, fill = nível_especialidade)) +
-   geom_bar(position = "fill") + 
+grafico_ESI_genero <-ggplot(especialidade, 
+                            aes(x = Genus, fill = nível_especialidade)) +
+  geom_bar(position = "fill") + 
   theme_minimal() +
   theme(axis.text.x = element_text(face = "italic", size = 12, angle = 45, hjust = 1)) +
   labs(
@@ -246,8 +263,7 @@ ggplot(especialidade,
     x = "Gêneros",
     y = "ESI em proporção"
   )
-
-
+ggsave("graficos/grafico_ESI_gen.png", plot = grafico_ESI_genero, width = 8, height = 6)
 
 ##informacao das legendas##
 #RR = Restricted range (global range size<50,000 km2 ) species get a 1
@@ -262,5 +278,13 @@ ggplot(especialidade,
 #ESI = Ecological Specialization Index: log10 (100/[dietary breadth x habitat breadth]), with a maximum of 2 for the most specialized species that only feed on one major food group and live in one major type of habitat (e.g., forest frugivore; Sekercioglu, 2011)
 
 
+## Exportar tabelas resumo
+write.xlsx(list(
+  "Contagem por gênero" = contagem_gen,
+  "Espécies ameaçadas" = dados_ameacados,
+  "Massa média por gênero" = media_massa_gen,
+  "Altitudes médias por gênero" = media_alt_gen,
+  "Especialização ecológica" = especialidade
+), file = "Resumo_Cracidae.xlsx")
 
 
